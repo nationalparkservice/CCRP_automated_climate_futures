09795b3c053c35d575655cfad2b8601cc08ae137
acrunyon <amber.c.runyon@gmail.com>
2022-07-18 17:26:41 -0600

    Change summary names from LT to timeseries

--- scripts/Summary plots.R
+++ scripts/Summary plots.R
@@ -25,23 +25,23 @@ WBAvgs$SWEaccum.inRoll10 <- rollmean(WBAvgs$SWEaccum.in, rollLen, fill = NA, ali
 
 # Tmean
 t<-LT_plot(yrAvgs,TavgF,rollvar=TavgRoll10,cols=col,yaxis="Mean annual temperature (\u00B0F)",title="") 
-ggsave("TavgF-LT.png",t, path = FigDir, height=PlotHeight, width=PlotWidth)
+ggsave("TavgF-Timeseries.png",t, path = FigDir, height=PlotHeight, width=PlotWidth)
 
 # Precip
 p<-LT_plot(yrAvgs,PrcpIn,rollvar=PrcpRoll10,cols=col,yaxis="Mean annual precipitation (inches/Yr)",title="")
-ggsave("PrcpIn-LT.png", p, path = FigDir, height=PlotHeight, width=PlotWidth)
+ggsave("PrcpIn-Timeseries.png", p, path = FigDir, height=PlotHeight, width=PlotWidth)
 
 # Deficit
 d<-LT_plot(WBAvgs,D.in,rollvar=D.inRoll10,cols=col,yaxis="Mean annual climatic water deficit (in/year)",title="")
-ggsave("D.in-LT.png", d, path = FigDir, height=PlotHeight, width=PlotWidth)
+ggsave("D.in-Timeseries.png", d, path = FigDir, height=PlotHeight, width=PlotWidth)
 
 # Runoff
 r<-LT_plot(WBAvgs, Runoff.in, rollvar=Runoff.inRoll10, cols=col,yaxis="Mean annual runoff (in/year)",title="")
-ggsave("Runoff.in-LT.png", r, path = FigDir, height=PlotHeight, width=PlotWidth)
+ggsave("Runoff.in-Timeseries.png", r, path = FigDir, height=PlotHeight, width=PlotWidth)
 
 # SWEaccum
 s<-LT_plot(WBAvgs, SWEaccum.in, rollvar=SWEaccum.inRoll10,cols=col,yaxis="Mean annual accumulated SWE (in/year)",title="")
-ggsave("SWEaccum.in-LT.png", path = FigDir, height=PlotHeight, width=PlotWidth)
+ggsave("SWEaccum.in-Timeseries.png", path = FigDir, height=PlotHeight, width=PlotWidth)
 
 
 ###### PANELS ##########
@@ -50,24 +50,24 @@ ggsave("SWEaccum.in-LT.png", path = FigDir, height=PlotHeight, width=PlotWidth)
 legend <- grid_arrange_shared_legend(t,p,nrow=2,ncol=1,position="bottom")
 g <- grid.arrange(legend,top = textGrob(paste0("Historical and future projections for ", SiteID),
                                         gp=gpar(fontface="bold", col="black", fontsize=26)))
-ggsave("TavgF-PrcpIn-LT.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
+ggsave("TavgF-PrcpIn-Timeseries.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
 
 #Temp and Deficit
 legend <- grid_arrange_shared_legend(t,d,nrow=2,ncol=1,position="bottom")
 g <- grid.arrange(legend,top = textGrob(paste0("Historical and future projections for ", SiteID),
                                         gp=gpar(fontface="bold", col="black", fontsize=26)))
-ggsave("TavgF_D.in-LT.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
+ggsave("TavgF_D.in-Timeseries.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
 
 #Temp and Runoff
 legend <- grid_arrange_shared_legend(t,r,nrow=2,ncol=1,position="bottom")
 g <- grid.arrange(legend,top = textGrob(paste0("Historical and future projections for ", SiteID),
                                         gp=gpar(fontface="bold", col="black", fontsize=26)))
-ggsave("TavgF_Runoff.in-LT.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
+ggsave("TavgF_Runoff.in-Timeseries.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
 
 #Temp and SWE
 legend <- grid_arrange_shared_legend(t,s,nrow=2,ncol=1,position="bottom")
 g <- grid.arrange(legend,top = textGrob(paste0("Historical and future projections for ", SiteID),
                                         gp=gpar(fontface="bold", col="black", fontsize=26)))
-ggsave("TavgF_SWEaccum.in-LT.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
+ggsave("TavgF_SWEaccum.in-Timeseries.png", g, path = FigDir, height=PanelHeight, width=PanelWidth)
 
 ##################
