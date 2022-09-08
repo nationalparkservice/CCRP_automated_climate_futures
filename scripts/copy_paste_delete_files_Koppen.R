d98ea753082a3c7ff372d2bfe7b10f8e535c4a21
acrunyon <amber.c.runyon@gmail.com>
2022-09-08 12:23:35 -0600

    Koppen names for copy_paste script

---
+++ scripts/copy_paste_delete_files_Koppen.R
@@ -0,0 +1,29 @@
+#######################################################
+##    COPIES FILES FROM CF REPO INTO LOCAL FOLDER   ###
+###             AND DELETES FROM REPO             #####
+#######################################################
+
+
+# Create folder structure locally
+
+# Scripts
+
+scripts_local_dir = paste(local_rss_dir, "scripts", sep = '/')
+if(dir.exists(scripts_local_dir) == FALSE){
+  dir.create(scripts_local_dir)
+}
+
+scripts_repo_dir <- paste(project_root_dir, "scripts", sep = '/')
+
+copyDirectory(from = scripts_repo_dir, to = scripts_local_dir, recursive = TRUE)
+
+# Figures and data
+copyDirectory(from = OutDir, to = local_rss_dir, recursive = TRUE)
+
+rmd <- list.files(project_root_dir, pattern = '.Rmd') 
+file.copy(file.path(project_root_dir, rmd), local_rss_dir)
+
+# Remove figures and park-specific data from repo
+base::unlink(paste0('./PARK/',Koppen), recursive = TRUE)
+
+
